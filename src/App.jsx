876497878e4a41eb13601cfec2349193eb6e07e1
zeeshanfreelancer <yourgithubemail@gmail.com>
2026-06-03 14:47:17 +0500

    added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import About from "./Component/about";
+
+export default function App(){
+  return(
+    <>
+    <h1>helloo</h1>
+    <About/>
+
+    
+    </>
+  )
+
+}
---
+++ src/Component/About.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <>
+            <div className="main">
+                {/* left */}
+                <div className="left">
+                    <img src="assets/img.jpg" alt="img" />
+                </div>
+                {/* right */}
+                <div className="right">
+About me
+                </div>
+            </div>
+
+
+        </>
+    )
+}
