d4836b168b9f400aa7cdf511bc6fdc4088a845f9
zeeshanfreelancer <yourgithubemail@gmail.com>
2026-06-12 16:04:08 +0500

    vercel_error

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import Navbar from "./Components/Navbar";
 // import Footer from "./components/Footer";
 
 
